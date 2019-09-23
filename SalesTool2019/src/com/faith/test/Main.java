cff8e54c5d5a25adc14a11d13d36293384cd758b
Acer PC <Acer PC@DESKTOP-Q7J4AK8>
2019-09-23 15:15:50 +0530

    initial commit

---
+++ SalesTool2019/src/com/faith/test/Main.java
@@ -0,0 +1,16 @@
+package com.faith.test;
+
+public class Main {
+
+	public static void main(String[] args){
+		SalesData obj = new SalesData();
+		System.out.println("Hello ust guys...");
+		displayGreeetings();
+		
+	}
+	
+	
+	private static void displayGreeetings(){
+		System.out.println("welcome");
+	}
+}
---
+++ SalesTool2019/src/com/faith/test/SalesData.java
@@ -0,0 +1,8 @@
+package com.faith.test;
+
+public class SalesData {
+
+	public void displaySalesGreetings(){
+		System.out.println("Hiiii....");
+	}
+}
